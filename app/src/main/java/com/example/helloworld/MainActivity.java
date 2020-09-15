799a863d68a73230ba49f7c83903a736e6447961
Albtool <albtoolin@albtwls-MacBook-Pro.local>
2020-09-15 16:34:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b ;
+    EditText E;
+    TextView v1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = (Button)findViewById(R.id.Hellobutton);
+        E =(EditText)findViewById(R.id.PersonName);
+        v1 = (TextView)findViewById(R.id.textView2);
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = E.getText().toString();
+                v1.setText("Hello "+ s);
+
+            }
+        });
+    }
+}
